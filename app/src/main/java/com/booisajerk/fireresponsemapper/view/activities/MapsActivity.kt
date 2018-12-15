a306046fab91c06050265891ce99ac8a1113d8a6
Jenn Parker <jenniferparker070@gmail.com>
2018-12-14 17:15:03 -0800

    Basic Map

---
+++ app/src/main/java/com/booisajerk/fireresponsemapper/view/activities/MapsActivity.kt
@@ -0,0 +1,76 @@
+package com.booisajerk.fireresponsemapper.view.activities
+
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.provider.SyncStateContract
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+import com.booisajerk.fireresponsemapper.R
+import com.booisajerk.fireresponsemapper.utils.DEFAULT_ZOOM
+import com.booisajerk.fireresponsemapper.utils.LOCATION_PERMISSION_REQUEST_CODE
+import com.booisajerk.fireresponsemapper.utils.SEATTLE_LOCATION
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+    override fun onMarkerClick(p0: Marker?) = false
+
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        map.uiSettings.isZoomControlsEnabled = true
+        map.setOnMarkerClickListener(this)
+
+        setUpMap()
+
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+            return
+        }
+
+        map.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, DEFAULT_ZOOM))
+            } else {
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(SEATTLE_LOCATION, DEFAULT_ZOOM))
+
+            }
+        }
+    }
+}
